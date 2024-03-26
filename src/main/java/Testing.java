68cf04a0bf63b621fe369897166268cfe1405261
PRMASCA <pradeep.mascarenhas@mercedes-benz.com>
2024-03-26 13:18:10 +0530

    initial commit

---
+++ src/main/java/Testing.java
@@ -0,0 +1,11 @@
+package main.java;
+
+public class Testing {
+
+    public static void main(String[] args) {
+        String secret="zfg92hvAPB";
+        String secret2="XnnMubIjBB";
+
+        System.out.println("secret: "+secret +" , secret2: "+secret2);
+    }
+}
