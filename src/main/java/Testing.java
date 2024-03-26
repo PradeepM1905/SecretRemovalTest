e852405bdd7f13f913407099e0326e7d1b7fc356
PRMASCA <pradeep.mascarenhas@mercedes-benz.com>
2024-03-26 13:27:45 +0530

    Commit from branch 2 second time

--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -12,5 +12,8 @@ public class Testing {
         String secret4="637DA528C2D1322A328EEC2EA12BB";
 
         System.out.println("secret3: "+secret3 +" , secret4: "+secret4);
+
+        String client_id="ce8bf0d9-1bd6-47ab-b470-ce86fdd40524";
+        System.out.println("client_id: "+client_id);
     }
 }
