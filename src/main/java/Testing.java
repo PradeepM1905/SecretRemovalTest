3e5d1666b163872513afa29288f3e63beefa2bcc
PRMASCA <pradeep.mascarenhas@mercedes-benz.com>
2024-03-27 19:39:04 +0530

    Removed secrets

--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -3,17 +3,17 @@ package main.java;
 public class Testing {
 
     public static void main(String[] args) {
-        String secret="zfg92hvAPB";
-        String secret2="XnnMubIjBB";
+        String secret="";
+        String secret2="";
 
         System.out.println("secret: "+secret +" , secret2: "+secret2);
 
-        String secret3="F2F7DACDE5265";
-        String secret4="637DA528C2D1322A328EEC2EA12BB";
+        String secret3="";
+        String secret4="";
 
         System.out.println("secret3: "+secret3 +" , secret4: "+secret4);
 
-        String client_id="ce8bf0d9-1bd6-47ab-b470-ce86fdd40524";
+        String client_id="";
         System.out.println("client_id: "+client_id);
     }
 }
